fa9ac748ef3c0ba878c0c023d27bbfb18e399c1f
PavloHorbatiuk <pavlo.horbatiuk@gmail.com>
2021-12-16 21:57:25 +0200

    counter

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import './App.module.css';
+import s from './Counter/Counter.module.css'
+import Counter from "./Counter/Counter";
+
+function App() {
+    return (
+        <div>
+            <Counter/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Counter/Counter.tsx
@@ -0,0 +1,59 @@
+import React, {ChangeEvent, useState} from 'react' ;
+import s from './Counter.module.css'
+
+
+function Counter() {
+    let [maxValue, setMaxValue] = useState(0)
+    let [count, setCount] = useState(0)
+    let [error, setError] = useState(false)
+
+    const setNumber = () => {
+
+    }
+    const maxValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+        let text = e.currentTarget.value
+
+    }
+    const minValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+    }
+    const incCallback = () => {
+
+        if (count < 5) {
+            setCount(count + 1)
+        } else {
+            setError(true)
+        }
+    }
+    const resetCallback = () => {
+        setCount(0)
+    }
+    return (
+        <div className={s.maincontainer}>
+            <div className={s.item}>
+                <div className={s.block1}>
+                    <div className={s.inputStyle1}>
+                        max value: <input type={"number"}
+                                          value={""}
+                                          onChange={maxValueChange}/>
+                        min value:<input type='number'
+                                         onChange={minValueChange}/>
+                    </div>
+                    <div className={s.setButton}>
+                        <button onClick={setNumber}>Set</button>
+                    </div>
+                </div>
+                <div className={s.block2}>
+                    <div className={`${s.sValue} ${error ? s.redNumber : ''}`}>{count}</div>
+                    <div className={s.inc_reset}>
+                        <button disabled={error} onClick={incCallback}>Inc</button>
+                        <button onClick={resetCallback}>Reset</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Counter
