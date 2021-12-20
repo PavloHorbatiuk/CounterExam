2d92ea7640ca547f63ce86345ddb6f5220fdf198
PavloHorbatiuk <pavlo.horbatiuk@gmail.com>
2021-12-20 15:42:56 +0200

    full counter

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,34 +1,56 @@
-import React, {ChangeEvent, useState} from 'react' ;
+import React, {ChangeEvent, useEffect, useState} from 'react' ;
 import s from './Counter.module.css'
 
 
 function Counter() {
-    const [maxvalue, setMaxvalue] = useState(0);
-    const [count, setCount] = useState(0);
     const [startNumber, setStartNumber] = useState(0);
+    const [maxValue, setMaxValue] = useState(0);
+    const [count, setCount] = useState(0);
     const [error, setError] = useState(false)
-    const [valueError, setvalueError] = useState<number | string>(0)
+    const [valueError, setValueError] = useState<number | string>(0)
 
+    useEffect(() => {
+        getLocalStorageValue();
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("maxValue", JSON.stringify(maxValue));
+        localStorage.setItem("startNumber", JSON.stringify(startNumber));
+    }, [maxValue,startNumber]);
+
+    const getLocalStorageValue = () => {
+        let storageStartValueAsString = localStorage.getItem("startNumber")
+        let storageMaxValueAsString = localStorage.getItem("maxValue")
+        if (storageMaxValueAsString) {
+            let newMaxValueFromStorage = JSON.parse(storageMaxValueAsString)
+            setMaxValue(newMaxValueFromStorage)
+            console.log(newMaxValueFromStorage)
+        }
+        if (storageStartValueAsString) {
+            let newStartValueFromStorage = JSON.parse(storageStartValueAsString)
+            setStartNumber(newStartValueFromStorage)
+        }
+    }
 
     const setNumber = () => {
-        setMaxvalue(maxvalue)
+        setMaxValue(maxValue)
         setCount(startNumber)
     }
     const maxValueChange = (e: ChangeEvent<HTMLInputElement>) => {
         let inputValue = parseInt(e.currentTarget.value);
-        (inputValue<0)? setvalueError("incorrect value") : setvalueError("Press set to install value");
-        (inputValue<0)?setError(true):setError(false)
-        setMaxvalue(inputValue)
+        (inputValue < 0) ? setValueError("incorrect value") : setValueError("Press set to install value");
+        (inputValue < 0) ? setError(true) : setError(false)
+        setMaxValue(inputValue)
     };
     const startValueChange = (e: ChangeEvent<HTMLInputElement>) => {
         let startValue = parseInt(e.currentTarget.value);
-        (startValue < 0) ? setvalueError("Incorrect value"):setvalueError("Press set to install value");
-        (startValue<0)? setError(true):setError(false)
+        (startValue < 0) ? setValueError("Incorrect value") : setValueError("Press set to install value");
+        (startValue < 0) ? setError(true) : setError(false)
         setStartNumber(startValue)
 
     }
     const incCallback = () => {
-        (count !== maxvalue) ? setCount(count + 1) : setError(true)
+        (count !== maxValue) ? setCount(count + 1) : setError(true)
     }
     const resetCallback = () => {
         setCount(0)
@@ -42,13 +64,13 @@ function Counter() {
                 <div className={s.block1}>
                     <div className={s.inputStyle1}>
                         Max value: <input
-                        // value="0"
+                       value={maxValue}
                         type="number"
                         onChange={maxValueChange}
                     />
                         Start value:<input
                         type='number'
-                        // value="0"
+                        value={startNumber}
                         onChange={startValueChange}/>
                     </div>
                     <div className={s.setButton}>
@@ -56,10 +78,10 @@ function Counter() {
                     </div>
                 </div>
                 <div className={s.block2}>
-                    <div className={error ? s.redNumber : s.sValue}>{count?count:valueError}</div>
+                    <div className={error ? s.redNumber : s.sValue}>{count ? count : valueError}</div>
                     <div className={s.inc_reset}>
                         <button disabled={error} onClick={incCallback}>Inc</button>
-                        <button  onClick={resetCallback}>Reset</button>
+                        <button onClick={resetCallback}>Reset</button>
                     </div>
                 </div>
             </div>
