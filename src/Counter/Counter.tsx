e60f7c0f43d2739a81d838a22cea9a56afe40443
PavloHorbatiuk <pavlo.horbatiuk@gmail.com>
2021-12-20 13:40:17 +0200

    counter finish

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -7,27 +7,28 @@ function Counter() {
     const [count, setCount] = useState(0);
     const [startNumber, setStartNumber] = useState(0);
     const [error, setError] = useState(false)
+    const [valueError, setvalueError] = useState<number | string>(0)
+
 
     const setNumber = () => {
         setMaxvalue(maxvalue)
         setCount(startNumber)
     }
     const maxValueChange = (e: ChangeEvent<HTMLInputElement>) => {
-        let inputValue = parseInt(e.currentTarget.value)
+        let inputValue = parseInt(e.currentTarget.value);
+        (inputValue<0)? setvalueError("incorrect value") : setvalueError("Press set to install value");
+        (inputValue<0)?setError(true):setError(false)
         setMaxvalue(inputValue)
     };
     const startValueChange = (e: ChangeEvent<HTMLInputElement>) => {
-        let startValue = parseInt(e.currentTarget.value)
+        let startValue = parseInt(e.currentTarget.value);
+        (startValue < 0) ? setvalueError("Incorrect value"):setvalueError("Press set to install value");
+        (startValue<0)? setError(true):setError(false)
         setStartNumber(startValue)
-        setError(false)
 
     }
     const incCallback = () => {
-        if (count < maxvalue) {
-            setCount(count + 1)
-        } else {
-            setError(true)
-        }
+        (count !== maxvalue) ? setCount(count + 1) : setError(true)
     }
     const resetCallback = () => {
         setCount(0)
@@ -40,31 +41,30 @@ function Counter() {
             <div className={s.item}>
                 <div className={s.block1}>
                     <div className={s.inputStyle1}>
-                        max value: <input
+                        Max value: <input
                         // value="0"
                         type="number"
                         onChange={maxValueChange}
                     />
-                        start value:<input
+                        Start value:<input
                         type='number'
                         // value="0"
                         onChange={startValueChange}/>
                     </div>
                     <div className={s.setButton}>
-                        <button onClick={setNumber}>Set</button>
+                        <button disabled={error} onClick={setNumber}>Set</button>
                     </div>
                 </div>
                 <div className={s.block2}>
-                    <div className={`${s.sValue} ${error ? s.redNumber : ''}`}>{count}</div>
+                    <div className={error ? s.redNumber : s.sValue}>{count?count:valueError}</div>
                     <div className={s.inc_reset}>
                         <button disabled={error} onClick={incCallback}>Inc</button>
-                        <button onClick={resetCallback}>Reset</button>
+                        <button  onClick={resetCallback}>Reset</button>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-
 
 export default Counter
